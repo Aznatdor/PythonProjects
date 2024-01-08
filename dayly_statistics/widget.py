5ccbd272704d98dd344d496fa1fc4c6e00fc04df
Aznatdor <andbob-ipt25@lll.kpi.ua>
2024-01-08 20:48:00 +0200

    New logic
    Now you can add new schedule and update exisiting ones.

--- dayly_statistics/widget.py
+++ dayly_statistics/widget.py
@@ -7,11 +7,11 @@ from PyQt5.QtWidgets import QWidget
 import json
 
 """
-Сделать добавление нового дня в папку. Сделать так, что бы при просмотре показывался тип знаятости, а при создании
-этот тип можно модифицировать. При нажатии на кнопку "Новый день" добавить проверку, что если день изменился, то создавать
+При нажатии на кнопку "Новый день" добавить проверку, что если день изменился, то создавать
 новый день. Делать проверку, чтобы после закрытия приложения кнопка "Новый день" не создавала новый день, если день
 ещё не изменился.
-Проведи тест показывания графиков. Добавить сериализацию и десериализацию.
+Проведи тест показывания графиков.
+Сделать удаление расписаний.
 
 Кешем будет открытые расписания, чтобы по новой из не создавать каждый раз. Если у меня такой прикол выйдет
 """
@@ -19,8 +19,6 @@ import json
 # ------------------------------------------------------------------------------------------------------------------#
 #                                                   Main Window                                                     #
 # ------------------------------------------------------------------------------------------------------------------#
-
-
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -51,12 +49,13 @@ class MainWindow(QtWidgets.QMainWindow):
         spacer.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         layout.addWidget(spacer)
 
-        # Field to enter name of the new schedule
+        '''# Field to enter name of the new schedule
         self.schedule_name = QtWidgets.QLineEdit()
-        layout.addWidget(self.schedule_name)
+        self.schedule_name.setWhatsThis('Это подсказка для поля ввода')
+        layout.addWidget(self.schedule_name)'''
 
         # Button to add new schedule
-        self.add_button = QtWidgets.QPushButton("Add Schedule")
+        self.add_button = QtWidgets.QPushButton("Add New Schedule")
         layout.addWidget(self.add_button)
         self.add_button.clicked.connect(self.add_schedule)
 
@@ -69,14 +68,75 @@ class MainWindow(QtWidgets.QMainWindow):
     #                                                   On-click methods                                                #
     # ------------------------------------------------------------------------------------------------------------------#
         
-    def add_schedule(self):
-        name = self.schedule_name.text()
 
-        # if name is an empty string or name already has been added, do nothing
-        if not name or self.schedules_list.findText(name) != -1:
-            return
-        self.schedule_name.clear() 
-        self.schedules_list.addItem(name)
+    def add_schedule(self):
+        '''
+        Creating window to manage schedule addition
+        '''
+        def confirm_additon():
+            name = schedule_name.text()
+
+            # if name is an empty string or name already has been added, do nothing
+            if not name or self.schedules_list.findText(name) != -1:
+                return
+            schedule_name.clear() 
+            self.schedules_list.addItem(name)
+            self.main_window.setDisabled(False)
+            self.inquire_box.close()
+
+            with open(r"ScheduleData\Schedules\{}.json".format(name), 'w') as json_file:
+                activities_list = activities_list_LineEdit.text().split(',')
+                json_dict = {
+                        "Activities" : activities_list,
+                        "Days" : []
+                    }
+                json.dump(json_dict, json_file, indent=4)
+
+            path = r"ScheduleData\Days\{}".format(name)
+            os.mkdir(path)
+                
+
+        # Creating main layout
+        self.inquire_box = QtWidgets.QWidget()
+        layout = QtWidgets.QVBoxLayout(self.inquire_box)
+
+        # Layout for schudule name LineEdit
+        name_layout = QtWidgets.QHBoxLayout()
+
+        # Activities layout
+        activities_layout = QtWidgets.QHBoxLayout()
+
+        # Adding layouyts
+        layout.addLayout(name_layout)
+        layout.addLayout(activities_layout)
+
+        # Creating LineEdit to get new schedule name
+        schedule_name = QtWidgets.QLineEdit()
+        schedule_name.setPlaceholderText('My Schedule')
+
+        # Adding description to the name LineEdit
+        name_description = QtWidgets.QLabel("Schedule name")
+        name_layout.addWidget(name_description)
+        name_layout.addWidget(schedule_name)
+
+        # Creating LineEdit to get new schedule activities
+        activities_list_LineEdit = QtWidgets.QLineEdit()
+        activities_list_LineEdit.setPlaceholderText('Анжумания, Пресс качат, Бегит')
+        activities_list_LineEdit.setToolTip("Названия активностей чрез запятую")
+
+        # Adding description to the activities list LineEdit
+        activities_description = QtWidgets.QLabel("Activities List")
+        activities_layout.addWidget(activities_description)
+        activities_layout.addWidget(activities_list_LineEdit)
+
+        # Ading confirmation button
+        confirm_button = QtWidgets.QPushButton("Confirm")
+        layout.addWidget(confirm_button)
+        confirm_button.clicked.connect(confirm_additon)
+
+        self.inquire_box.show()
+
+        self.main_window.setDisabled(True)
 
     def delete_schedule(self):
         name = self.schedules_list.currentText()
@@ -117,8 +177,9 @@ class ScheduleManager(QtWidgets.QMainWindow):
 
     def init_ui(self, schedule_name, main_window):
         # getting data
+        self.activities = None
         self.schedule = self.deserialize_schedule(schedule_name)
-        self.new_day = None
+        self.days = []
 
         self.resize(500, 700)
         self.setWindowTitle("Schedule Manager: " + schedule_name)
@@ -148,10 +209,20 @@ class ScheduleManager(QtWidgets.QMainWindow):
         self.view_day_button.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         self.layout.addWidget(self.view_day_button)
 
+        # Button to update scedule
+        '''
+        Сделай разделение на папки в днях, чтобы можно было сохранять одинаковые дни разных расписаний
+        '''
+        self.update_button = QtWidgets.QPushButton("Обновить", self)
+        self.update_button.clicked.connect(self.update_schedule)
+        self.update_button.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.layout.addWidget(self.update_button)
+
         # Button to return to main window
         self.return_button = QtWidgets.QPushButton("Вернуться", self)
         self.return_button.clicked.connect(self.return_home)
         self.layout.addWidget(self.return_button)
+
 
 
 # ------------------------------------------------------------------------------------------------------------------#
@@ -159,12 +230,13 @@ class ScheduleManager(QtWidgets.QMainWindow):
 # ------------------------------------------------------------------------------------------------------------------#
         
     def add_new_day(self):
-        if self.new_day is None:
+        if self.days == []:
             today_date = QtCore.QDate.currentDate()
-            self.new_day = DayManager(today_date.toString("dd.MM.yyyy"), 
+            new_day = DayManager(today_date.toString("dd.MM.yyyy"), 
                                         self.schedule_name, self, False)
+            self.days.append(new_day)
         
-        self.new_day.show()
+        self.days[-1].show()
         self.setVisible(False)
 
 
@@ -181,7 +253,8 @@ class ScheduleManager(QtWidgets.QMainWindow):
         self.setVisible(False) # Закрываем основное окно
 
     def update_schedule(self):
-        pass
+        for day in self.days:
+            day.serialize_day()
 
     def deserialize_schedule(self, schedule_name):
         schedule = None
@@ -189,14 +262,14 @@ class ScheduleManager(QtWidgets.QMainWindow):
         with open(r'ScheduleData\Schedules\{}.json'.format(schedule_name), 'r') as schedule_file:
             schedules_dict = json.load(schedule_file)
             schedule = classes.TimeStatistics(schedules_dict["Activities"])
-            activities = schedules_dict["Activities"]
+            self.activities = schedules_dict["Activities"]
 
             for day in schedules_dict["Days"]:
-                with open(r'ScheduleData\Days\{}.json'.format(day), 'r') as day_file:
+                with open(r'ScheduleData\Days\{0}\{1}.json'.format(schedule_name, day), 'r') as day_file:
                     day_dict = json.load(day_file)
                     schedule.begin_day(day)
 
-                    for activity in activities:
+                    for activity in self.activities:
                         for time_stamp in day_dict[activity]:
                             schedule.add_timestamp(activity, classes.TimeStamp(*time_stamp))
 
@@ -269,15 +342,15 @@ class CalendarWindow(QWidget):
         
 
 class DayManager(QtWidgets.QWidget):
-    def __init__(self, date, schedule_name, main_window, deserialize):
+    def __init__(self, date, schedule_name, schedule_window, deserialize):
         super().__init__()
-        self.init_ui(date, schedule_name, main_window, deserialize)
+        self.init_ui(date, schedule_name, schedule_window, deserialize)
 
-    def init_ui(self, date : str, schedule_name : str, main_window,  deserialize : bool):
+    def init_ui(self, date : str, schedule_name : str, schedule_window,  deserialize : bool):
         # Resizing
         self.resize(500, 700)
-        self.setWindowTitle("Day Manager: " + schedule_name)
-        self.main_window = main_window
+        self.setWindowTitle("Day Manager: " + date)
+        self.schedule_window = schedule_window
         self.date = date
         self.deserialize_flag = deserialize
 
@@ -302,6 +375,7 @@ class DayManager(QtWidgets.QWidget):
         self.return_button.clicked.connect(self.return_home)
         self.edit_time_points_layout.addLayout(self.tool_box)
 
+        # Adding button to show distribution if we're deserializing day
         if self.deserialize_flag:
             self.show_time_distribution_button = QtWidgets.QPushButton("Показать распределение", self)
             self.tool_box.addWidget(self.show_time_distribution_button)
@@ -314,12 +388,21 @@ class DayManager(QtWidgets.QWidget):
 
         # Adding "spacer" widget
         self.spacer = QtWidgets.QWidget()
-        self.spacer.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
+        self.spacer.setSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         self.description_layout.addWidget(self.time_column_name)
         self.description_layout.addWidget(self.spacer)
         self.description_layout.addWidget(self.comments_column_name)
 
+        # Another spacer
+        self.spacer2 = QtWidgets.QWidget()
+        self.spacer2.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.layout.addWidget(self.spacer2)
+
         self.line_edits = [] # To properly add points
+        self.activities_marker = [] # To properly add actvity type of the given timestamp
+
+        # Dict for timestamp to activity map
+        self.time_stamp_to_activity = dict()
         if not self.deserialize_flag:
             # Adding button to add QLineEdits
             self.add_button = QtWidgets.QPushButton("Добавить QLineEdit", self)
@@ -345,19 +428,40 @@ class DayManager(QtWidgets.QWidget):
 # ------------------------------------------------------------------------------------------------------------------#
 #                                                   Methods                                                         #    
 # ------------------------------------------------------------------------------------------------------------------#
+    def add_activity_type(self, time_stamp : tuple):
+        '''
+        Method to add activity type to the corresponding day entry
+        '''
+        for activity_mark in self.activities_marker:
+            if activity_mark.text() == "":
+                activity_mark.setText(self.time_stamp_to_activity[time_stamp])
+                break
+
 
     def add_line_edits(self):
+        '''
+        Method to add new day entry
+        '''
         # Creating layout of 2 line edits and one text edit
         line_edit_layout = QtWidgets.QHBoxLayout()
 
         # Widgets to add
         line_edit1 = QtWidgets.QLineEdit()
         line_edit2 = QtWidgets.QLineEdit()
-        text_edit = QtWidgets.QTextEdit()
-        activites_list = QtWidgets.QComboBox()
+        text_edit = QtWidgets.QLineEdit()
 
-        for activity in self.main_window.schedule.activity_names:
-            activites_list.addItem(activity)
+        if not self.deserialize_flag:
+            # if we're managing new day, add Combobox
+            activites_list = QtWidgets.QComboBox()
+            # filling up activities ComboBox 
+            for activity in self.schedule_window.schedule.activity_names:
+                activites_list.addItem(activity)
+        else:
+            # if we're looking at the past day, add LineEdit
+            activites_list = QtWidgets.QLineEdit()
+            activites_list.setReadOnly(True)
+        self.activities_marker.append(activites_list)
+
 
         # Setting line edit widgets to read only mode
         line_edit1.setReadOnly(True)
@@ -381,7 +485,10 @@ class DayManager(QtWidgets.QWidget):
         self.edit_time_points_layout.addLayout(line_edit_layout)
 
     def add_text_to_last_edit(self, time = False):
-        if not time:
+        '''
+        Method to add timestamps to the last empty LineEdit
+        '''
+        if time == False:
             current_time = datetime.today()
             # Prettifying minute part of string appearance
             minutes = str(current_time.minute) if current_time.minute > 9 else "0" + str(current_time.minute) 
@@ -396,45 +503,108 @@ class DayManager(QtWidgets.QWidget):
                 break
     
     def return_home(self):
-        self.main_window.setVisible(True)  # Показываем второе окно
+        self.schedule_window.setVisible(True)  # Показываем второе окно
         self.setVisible(False) # Закрываем основное окно
+
+    def time_stamp_to_str(self, time_stamp : list):
+        '''
+        To convert time_stamp list into pretty string
+        '''
+        begin, end = time_stamp
+
+        begin = begin[:]
+        end = end[:]
+
+        if int(begin[1]) < 10:
+            begin[1] = "0" + str(begin[1])
+        if int(end[1]) < 10:
+            end[1] = "0" + str(end[1])
+
+        str1, str2 = ":".join([str(begin[0]), str(begin[1])]), ":".join([str(end[0]), str(end[1])])
+        return str1, str2
+    
+    def str_to_timestamp(self, time_stamp_str1, time_stamp_str2):
+        '''
+        To conver time_stamp string into list
+        '''
+        if time_stamp_str1 == '' or time_stamp_str2 == '':
+            return
+        
+        time_stamp1 = time_stamp_str1.split(':')
+        time_stamp2 = time_stamp_str2.split(':')
+
+        time_stamp_list1 = list(map(int, time_stamp1))
+        time_stamp_list2 = list(map(int, time_stamp2))
+
+        return [time_stamp_list1, time_stamp_list2]
+
 
     def deserialize(self):
         """
-        Showing time. Without specific activity
+        Showing time of the given day
         """
-        folder_path = r"ScheduleData\Days"
+        schedule_name = self.schedule_window.schedule_name
+        folder_path = r"ScheduleData\Days\{}".format(schedule_name)
         file_names = os.listdir(folder_path)
 
+
         if (self.date + ".json") in file_names:
-            with open(r"ScheduleData\Days\{}.json".format(self.date), "r") as day:
+            with open(r"ScheduleData\Days\{0}\{1}.json".format(schedule_name, self.date), "r") as day:
                 data = json.load(day)
 
                 time_stamps = []
                 for activity in data:
-                    for time_stamp in data[activity]:
-                        time_stamps.append(time_stamp)
+                    for timeStamp in data[activity]:
+                        # to be able to map
+                        time_stamp_string = self.time_stamp_to_str(timeStamp)
+                        self.time_stamp_to_activity[time_stamp_string] = activity
+                        time_stamps.append(timeStamp)
 
                 time_stamps.sort()
 
                 for i in range(len(time_stamps)):
                     self.add_line_edits()
 
-                for time_stamp in time_stamps:
-                    begin, end = time_stamp
+                for timeStamp in time_stamps:
+                    begin, end = self.time_stamp_to_str(timeStamp)
 
-                    if begin[1] < 10:
-                        begin[1] = "0" + str(begin[1])
-                    if end[1] < 10:
-                        end[1] = "0" + str(end[1])
-                    
-                    self.add_text_to_last_edit(":".join([str(begin[0]), str(begin[1])]))
-                    self.add_text_to_last_edit(":".join([str(end[0]), str(end[1])]))                 
+                    self.add_text_to_last_edit(begin)
+                    self.add_text_to_last_edit(end) 
+                    self.add_activity_type((begin, end))                
         else:
             self.return_home()
 
     def serialize_day(self):
-        pass
+        '''
+        Method to covert day into json file
+        '''
+        json_dict = {activity : [] for activity in self.schedule_window.activities}
+
+        for entry_num in range(len(self.activities_marker)):
+            time_stamp_str1 = self.line_edits[2 * entry_num].text()
+            time_stamp_str2  = self.line_edits[2 * entry_num + 1].text()
+
+            activity = self.activities_marker[entry_num].currentText()
+
+            time_stamp_list = self.str_to_timestamp(time_stamp_str1, time_stamp_str2)
+            if time_stamp_list is not None:
+                json_dict[activity].append(time_stamp_list)
+
+        day_file_path = r"ScheduleData\Days\{0}\{1}.json".format(self.schedule_window.schedule_name, self.date)
+
+        # you can use 'x' instead of 'w' to prevent rewriting
+        with open(day_file_path, 'w') as json_file:
+            json.dump(json_dict, json_file, indent=4)
+
+        schedule_file_path = r'ScheduleData\Schedules\{}.json'.format(self.schedule_window.schedule_name)
+        with open(schedule_file_path, 'r+') as json_file:
+            # Loading data
+            data = json.load(json_file)
+            data["Days"].append(self.date)
+
+            # moving pointer to the begining of the file
+            json_file.seek(0)
+            json.dump(data, json_file, indent=4)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
